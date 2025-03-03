565b48a167efa08992cef96e3d24efceb9d95827
Antonio SJ Musumeci <trapexit@spawn.link>
2025-03-02 22:56:22 -0600

    tdo_file_stream.cpp

--- src/tdo_file_stream.cpp
+++ src/tdo_file_stream.cpp
@@ -40,7 +40,7 @@ namespace TDO
 
     _fs.open(filepath_,std::ios::binary|std::ios::in|std::ios::out);
     if(!_fs)
-      return "failure";
+      return "failed to open";
     err = setup();
     if(err)
       return err;
