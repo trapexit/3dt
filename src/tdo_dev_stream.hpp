0ad35390caa0857eee1322ff0c9e20c0839dfdff
Antonio SJ Musumeci <trapexit@spawn.link>
2025-03-04 22:26:10 -0600

    tdo_dev_stream.hpp

--- src/tdo_dev_stream.hpp
+++ src/tdo_dev_stream.hpp
@@ -154,9 +154,9 @@ namespace TDO
     void write(const TDO::DirectoryRecord &);
     void write(const TDO::ROMTag &);
     void write(const TDO::LinkedMemFileEntry &);
-    void write_data_blocks(std::vector<char> &v,
-                           const s64          block_pos,
-                           const s64          blocks);
+    void write_data_blocks(const std::vector<char> &v,
+                           const s64                block_pos,
+                           const s64                blocks);
     void write_data_bytes_from_block(std::vector<char> &v,
                                      const s64          block_pos,
                                      const s64          bytes);
