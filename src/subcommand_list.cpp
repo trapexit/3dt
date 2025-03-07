5e30e450bfce4e53b3f6c592b6f114101e38bae2
Antonio SJ Musumeci <trapexit@spawn.link>
2025-03-07 00:20:18 -0600

    subcommand_list.cpp

--- src/subcommand_list.cpp
+++ src/subcommand_list.cpp
@@ -212,7 +212,7 @@ namespace Subcommand
     ListCallbacks callbacks(opts_);
     TDO::FSWalker walker(fs,callbacks);
 
-    fs.open(opts_.filepath,fs.binary|fs.in);
+    fs.open(opts_.filepath,std::ios::binary|std::ios::in);
     if(!fs.good())
       return  Log::error_stream_open(opts_.filepath);
 
