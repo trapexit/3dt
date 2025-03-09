2f52282c008e3d7b09f33f7a998087546cdb32e9
Antonio SJ Musumeci <trapexit@spawn.link>
2025-03-09 10:10:22 -0500

    tdo_dev_stream.cpp

--- src/tdo_dev_stream.cpp
+++ src/tdo_dev_stream.cpp
@@ -134,6 +134,8 @@ TDO::DevStream::setup()
       find_label();
       if(eof())
         return {"unable to find OperaFS in image"};
+
+      read(dl);
       
       _device_block_header    = 0;
       _device_block_data_size = _disc_label.volume_block_size;
