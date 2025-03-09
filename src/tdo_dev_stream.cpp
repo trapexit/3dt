83ba4e1e36e32bb7b6abe0aeb09fb33bc590e720
Antonio SJ Musumeci <trapexit@spawn.link>
2025-03-09 12:15:27 -0500

    tdo_dev_stream.cpp

--- src/tdo_dev_stream.cpp
+++ src/tdo_dev_stream.cpp
@@ -249,7 +249,7 @@ TDO::DevStream::romtag(const int type_)
       TDO::ROMTag romtag;
 
       read(romtag);
-      fmt::print("{}\n",romtag.type);
+      fmt::print("{:x}\n",romtag.type);
       if(romtag.type == type_)
         return romtag;
       if((romtag.type == 0) || (romtag.sub_systype == 0))
