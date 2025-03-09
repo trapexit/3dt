6824570a366993108d07f6a10a0b228243e12403
Antonio SJ Musumeci <trapexit@spawn.link>
2025-03-09 17:35:24 -0500

    subcommand_sign.cpp

--- src/subcommand_sign.cpp
+++ src/subcommand_sign.cpp
@@ -63,7 +63,7 @@ public:
     if(lc_filepath != "signatures")
       return;
 
-    stream_.data_byte_seek(record_pos_);
+    s_.data_byte_seek(record_pos_);
     s_.data_byte_skip(offsetof(TDO::DirectoryRecord,byte_count));
     s_.write(signatures_file_size);
     s_.write((u32)((signatures_file_size + (2048 - 1)) / 2048));
