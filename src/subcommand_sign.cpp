c844e6d979981a4468b509f17ca4b875772118b1
Antonio SJ Musumeci <trapexit@spawn.link>
2025-03-09 22:37:35 -0500

    subcommand_sign.cpp

--- src/subcommand_sign.cpp
+++ src/subcommand_sign.cpp
@@ -166,6 +166,11 @@ _generate_and_write_romtags(TDO::FileStream &s_)
   TDO::ROMTagVec sorted;
 
   sorted.emplace_back(s_.romtag(0x0D));
+  sorted.emplace_back(s_.romtag(0x07));
+  sorted.emplace_back(s_.romtag(0x0C));
+  sorted.emplace_back(s_.romtag(0x02));
+  sorted.emplace_back(s_.romtag(0x10));
+  sorted.emplace_back(s_.romtag(0x14));  
 }
 
 
