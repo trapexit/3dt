363a30a3442fbacb71831db62a0343d280c4fe14
Antonio SJ Musumeci <trapexit@spawn.link>
2025-03-17 23:22:19 -0500

    subcommand_sign.cpp

--- src/subcommand_sign.cpp
+++ src/subcommand_sign.cpp
@@ -259,11 +259,11 @@ _sign_signature_block(TDO::FileStream &s_)
     {
       s64 block_pos;
 
-      s_.good();
-      
       block_pos = ((i * LOG_BLOCK_SIZE) / PHY_BLOCK_SIZE);
 
       buf.clear();
+      fmt::print("{} {}\n",block_pos,s_.good());
+      
       s_.read_data_blocks(buf,block_pos,(LOG_BLOCK_SIZE / PHY_BLOCK_SIZE));
 
       md5_calc(buf.data(),buf.size(),digest);
