a8b409ce6f1edc69d80190412e88e49133b9437d
Antonio SJ Musumeci <trapexit@spawn.link>
2025-03-23 10:30:36 -0500

    subcommand_sign.cpp

--- src/subcommand_sign.cpp
+++ src/subcommand_sign.cpp
@@ -425,6 +425,7 @@ namespace Subcommand
 
         ::_pad_image_and_update_disclabel(stream);
         ::_generate_and_write_romtags(stream);
+        ::_add_3dt_mark(stream);
         ::_sign_appsplash(stream);
         ::_generate_sign_resize_signatures_file(stream);
         ::_sign_disclabel_romtags_bootcode(stream);
